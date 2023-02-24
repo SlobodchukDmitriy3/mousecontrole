fb47a6f2390d08962f7fddd2f7c91401c6f00b67
Dmitriy Slobodchuk <104769679+SlobodchukDmitriy3@users.noreply.github.com>
2023-02-24 20:15:12 +0200

    Add files via upload

---
+++ mousecontrole.py.py
@@ -0,0 +1,28 @@
+from tkinter import *
+
+
+def b1(event):
+    print("Pushed right button of the mouse")
+
+
+def b3(event):
+    print("Pushed left button of the mouse")
+
+
+def move(event):
+    x = event.x
+    y = event.y
+    s = "Move the mouse {}x{}".format(x, y)
+    root.title(s)
+    root = Tk()
+    root.minsize(width=400, height=300)
+    root.bind('<Button-3>', b1)
+    root.bind('<Button-1>', b3)
+    root.bind('<Motion>', move)
+    root.mainloop()
+
+
+
+
+
+    
